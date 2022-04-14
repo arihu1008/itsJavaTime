72de59a2e57edf039b02c8f9268bc7a5413d49f8
Yooboo Park <yooboopark@Yooboos-Air.home>
2022-04-14 11:12:27 -0500

    printing initials

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,9 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("Y   Y B B");
+        System.out.println(" Y Y  B  B");
+        System.out.println("  Y   B B");
+        System.out.println("  Y   B  B");
+        System.out.println("  Y   B B");
+    }
+}
