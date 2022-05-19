4fe6163027ab4fd5df1ed6d8f8633393fc369668
Yooboo Park <yooboopark@Yooboos-Air.home>
2022-05-19 11:26:12 -0500

    type:long

---
+++ section two/Form.java
@@ -0,0 +1,9 @@
+public class Form {
+  public static void main(String[] args) {
+    //char can only store single character
+    //char consumes less memory and is performance is faster than String
+    char gender = 'F';
+    System.out.println("Gender: " + gender);
+
+  }
+}
